526d2bae59eebef6f090491f6323132c416880a9
serge <csuigres@gmail.com>
2020-09-03 15:00:55 +0300

    #111111 [Src]: First commit

---
+++ new/script.js
@@ -0,0 +1,19 @@
+
+var obj = {
+    name:"Ion",
+    hz:{
+        obj:[1,'1',new Date()]
+    },
+    sayName: function(){
+        return this.name;
+    },
+    setName: function(name){
+        this.name=name;
+    }
+};
+
+let a = "123lka";
+alert(parseInt(a));
+
+
+
