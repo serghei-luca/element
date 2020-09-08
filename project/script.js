d51c72390139df4980d31e46255b482512c92ad3
serge <hipertalamus.gmail.com>
2020-09-08 18:27:13 +0300

    #008 Generalizare

--- project/script.js
+++ project/script.js
@@ -1,19 +1,36 @@
+let obiect ={
+    buget:0, timeData:0, 
+    expenses:{
+        prop1:"",prop2:""
+    },
+    optionalExpenses:{
 
-var obj = {
-    name:"Ion",
-    hz:{
-        obj:[1,'1',new Date()]
     },
-    sayName: function(){
-        return this.name;
-    },
-    setName: function(name){
-        this.name=name;
-    }
+    income:[],
+    savings: false,  
 };
+let date = new Date();
 
-let a = "123lka";
-alert(parseInt(a));
+let inp = prompt("Care e bugetul Dvs lunar?", '0 USD');
+obiect.buget=parseFloat(inp);
 
+inp = prompt("Indicati data in format YYYY-MM-DD", date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate());
+obiect.timeData=new Date(inp);
+
+inp = prompt("Mai am nevoie de ceva:\nDati o alta data in care ati cheltuit:", new Date().toString("YYYY-MM-DD"));
+obiect.expenses.prop1=new Date(inp);
+inp = prompt("Care e bugetul din acea lună?", '0 USD');
+obiect.expenses.prop2=parseFloat(inp);
+
+let sb = "Buget lunar: "+obiect.buget+"<br>";
+sb+="Data: "+obiect.timeData+"<br>";
+sb+=obiect.expenses.prop1+" : ";
+sb+=obiect.expenses.prop2+"<br>";
+sb+="<p>Bugetul dvs zilnic este:"+(obiect.buget/30)+" USD</p>";
+console.log(sb);
+var el = document.getElementById("demo");
+if(el){
+    el.innerHTML=sb;
+}
 
 
